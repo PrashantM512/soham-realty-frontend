e0f384b2ab6504e56fc10b03af87f9fc17f9083b
PrashantM512 <prashantmorade512@gmail.com>
2025-07-24 12:59:10 +0530

    env variable update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://soham-realty-backend.onrender.com/api'
-
+    apiUrl: 'https://soham-realty-backend.onrender.com/api',
+    imageUrl: 'https://soham-realty-backend.onrender.com'
   };
